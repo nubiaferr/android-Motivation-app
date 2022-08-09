35c8656688279248183cfc8408c5739996ae0684
Nubia <nubiaferrsvd@gmail.com>
2022-08-09 01:22:51 -0300

    feat: motivation app

---
+++ app/src/main/java/com/nubia/motivation/ui/UserActivity.kt
@@ -0,0 +1,64 @@
+package com.nubia.motivation.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.nubia.motivation.infrastructure.MotivationConstants
+import com.nubia.motivation.R
+import com.nubia.motivation.infrastructure.SecurityPreferences
+import com.nubia.motivation.databinding.ActivityUserBinding
+
+class UserActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityUserBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityUserBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonSave.setOnClickListener(this)
+
+        supportActionBar?.hide()
+
+        verifyUserName()
+
+    }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.button_save) {
+            handleSave()
+        }
+    }
+
+    private fun verifyUserName() {
+        val name = SecurityPreferences(this).getString(
+            MotivationConstants.KEY.USER_NAME
+        )
+
+        if (name != "") {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+    }
+
+    private fun handleSave() {
+        val name = binding.editName.text.toString()
+
+        if (name != "") {
+
+            SecurityPreferences(this).storeString(MotivationConstants.KEY.USER_NAME, name)
+
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        } else {
+            Toast.makeText(
+                this,
+                R.string.validation_mandatory_name,
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+}
